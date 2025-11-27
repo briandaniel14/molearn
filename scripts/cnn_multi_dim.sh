0c3118af77f1637362173e40aa1859082c5b8f04
briandaniel14 <briandaniel789@gmail.com>
2025-11-27 12:58:18 +0000

    added script for cnn_multi_dim

---
+++ scripts/cnn_multi_dim.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+
+export MOLEARN_PATH=/home/${USER}/repos/molearn
+export SCRATCH_HOME=/disk/scratch/${USER}/molearn
+export OUTPUT_SCRATCH=${SCRATCH_HOME}/examples/cnn_multi_dim_checkpoints
+export OUTPUT_DIST=/home/${USER}/results/${TIMESTAMP}
+
+mkdir -p $OUTPUT_DIST
+
+mkdir -p ${SCRATCH_HOME}
+rsync --archive --update --compress --progress ${MOLEARN_PATH}/ ${SCRATCH_HOME}
+
+echo 'unzipping data'
+cd ${SCRATCH_HOME}/examples/data
+
+tar -xzf MurD_closed_apo.tar.gz
+tar -xzf MurD_closed.tar.gz
+tar -xzf MurD_open.tar.gz
+
+cd ..
+
+source /home/${USER}/miniconda3/bin/activate molearn
+
+echo 'running script'
+python runner_CNN_multi_dim.py
+
+rsync --archive --update --compress --progress ${OUTPUT_SCRATCH} ${OUTPUT_DIST}
+
+echo 'cleaning up'
+cd /disk/scratch/
+rm -r -f s2305437
+echo 'cleanup complete'
+
+echo "Job is done"
+exit 0

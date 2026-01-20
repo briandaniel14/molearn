5bde96f0549c447ea766092a8a348cd71791c652
briandaniel14 <briandaniel789@gmail.com>
2026-01-20 00:03:38 +0000

    changed latent_z to 6

--- scripts/runner_CNN_multi_dim.py
+++ scripts/runner_CNN_multi_dim.py
@@ -59,7 +59,7 @@ def main():
 
         print(f'running CNN autoencoder model with latent dim: {d}')
         torch.manual_seed(0)
-        model = AutoEncoder(latent_dim=d)
+        model = AutoEncoder(latent_dim=d, latent_z=6)
         trainer.set_autoencoder(model, out_points=data.dataset.shape[1])
         trainer.prepare_optimiser()
 
